2b38400b3a9b582c2c7704eab75102e9dffcf137
HemaMaruthi <Smaruthi.mahi111@gmail.com>
2024-09-21 21:17:31 +0530

    Day 11

---
+++ JavaScript/Day 11/dataType.js
@@ -0,0 +1,67 @@
+// var greet = "Hello, Good Evening";
+// var message = "Welcome to \t JavaScript Live Class";
+// var messageNew = "Welcome to \n JavaScript Live Class";
+// var message = "Welcome to \r JavaScript Live Class";
+// var message = "Welcome to \v JavaScript Live Class";
+// console.log(greet);
+// console.log(message);
+// console.log(greet +" "+           message);
+// console.log(messageNew)
+// console.log(message)
+// console.log(greet.length)
+// console.log(greet.charAt(7))
+
+// var greet = "Hello, Good Evening";
+// console.log(greet.toUpperCase())
+// console.log(greet.toLowerCase())
+
+// var number = "123";
+// var num = parseInt("123.1336")
+// var num = parseFloat("123.134")
+
+// console.log(num.toFixed(3))
+// var num = "3";
+// var num1 = parseInt(num)
+// console.log(num1)
+// console.log(1 - "3")
+// console.log(1 + "3")
+
+// console.log(1 + "1" + "2")
+// console.log(1 + "1" - "2")
+// console.log(1 - "1" + "2")
+// console.log(1 + + "1" + "2")// 1 12
+// 1 + 1 + "2" = 2+"2" = 22
+// console.log(1 - - "1" + "2") // 12 // -12
+// 1-(-1) = 2 + "2" = 22
+// console.log("A" + "B")
+// console.log("A" - "B")
+// console.log(2 ** * "2")
+// console.log(2 * -"2")
+// console.log(2 * +"2")
+
+// console.log(Math.sqrt(65))
+// console.log(Math.abs(-2))
+// console.log(Math.min(2,56,8,2,69))
+// console.log(Math.max(2,56,8,2,69))
+
+// var isMarried = false;
+// console.log(isMarried)
+
+// var age = 18;
+// console.log(age == 20)
+
+// var num = Boolean(-2256)
+// console.log(num)
+
+var num = null;
+console.log(num)
+
+
+
+
+
+
+
+
+
+
