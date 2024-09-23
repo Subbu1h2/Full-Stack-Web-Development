1e81f1e3ef8b206626dbc824497c37b3a0018f4d
HemaMaruthi <Smaruthi.mahi111@gmail.com>
2024-09-23 21:28:40 +0530

    Day 12

---
+++ JavaScript/Day 12/operator.js
@@ -0,0 +1,123 @@
+// console.log(2 + 3)
+// console.log( 3 * 4)
+// console.log( 4 % 2)
+// console.log( 2 ** 3) 2 *2*2 = 8
+
+// var x = 1;
+// var y = x + 2;
+// console.log(y);
+
+// var x  = 1;
+// x = x + 2
+// x = + 2
+// x += 2 
+// x -= 2 
+// x = x -2 ;
+// x = 1 - 2 = -1
+// var x = 2
+// x *=2;
+// x /=2;
+// x = x/ 2 = 2/2  = 1 ;
+// x = x * 2 => 2 * 2 = 4
+// x %= 2;
+
+// x = x % 2 = 2 % 2 = 0
+// console.log(x)
+
+
+// console.log( 2 === "2")
+        // dataType : Value
+        //  number : 2 
+        // String : "2"
+// console.log(undefined === null)
+
+// console.log( 2 != 3)
+// console.log(2 <= 3)
+// console.log(2 != 4 , 2 >=3)
+
+// var isWarm = true;
+// var isRainy =  false;
+
+// console.log(isWarm && isRainy)
+// console.log(isWarm || isRainy)
+
+    // true && false
+
+    // console.log(0 & 1)
+
+    // console.log( 5 & 3)
+        //  5 = 1 0 1 
+        //         &
+        //  3 = 0 1 1
+        //  ---------------
+        //     0 0 1
+
+        // console.log( 5 | 3) // 1
+
+    //     5 = 1 0 1 
+    //          |
+    //     3 = 0 1 1
+    //    --------------------
+    //         1 1 1 
+
+    // x++;
+    // ++x;
+    // var y = x++;
+    // var y = ++x;
+    
+    // console.log(y)
+    // var x = {};
+    // 000
+    // 000
+
+    // console.log(typeof x)
+    // console.log(typeof null)
+    // console.log(typeof [])
+    // console.log(typeof {})
+
+    // console.log("1");
+    // console.log("4");
+    // console.log("2");
+    // console.log("3");
+
+    // var trafficLight= "Red";
+    // var trafficLight= "Green";
+    // var trafficLight= "Yellow";
+    // console.log(trafficLight)
+// console.log(trafficLight == "Green")
+    // if(trafficLight == "Green"){
+    //     console.log("Light is Green: Move")
+    // }
+    // else{
+    //     console.log("Please Stop")
+    // }
+    // var trafficLight= "Red";
+    // var trafficLight= "Green";
+    // var trafficLight= "Yellow";
+    // if(trafficLight == "Green"){
+    //     console.log("Light is Green: Move")
+    // }
+    // else if(trafficLight == "Red"){
+    //     console.log("Please Stop")
+    // }
+    // else if(trafficLight == "Yellow"){
+    //     console.log("Light is Yellow : Get Ready")
+    // }
+
+ // var trafficLight= "Red";
+    var trafficLight= "Green";
+    // var trafficLight= "Yellow";
+    // if(trafficLight == "Green"){
+        //     console.log("Light is Green: Move")
+        // }
+        // else{
+        //     console.log("Please Stop")
+        // }
+
+   console.log((trafficLight == "Yellow") ? "Light is Green: Move" : "Please Stop");
+
+
+
+
+
+
