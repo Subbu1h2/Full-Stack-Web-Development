7bce596f1aad9660eba2ece424e268a218991341
HemaMaruthi <Smaruthi.mahi111@gmail.com>
2024-09-25 21:06:10 +0530

    Day 14

---
+++ JavaScript/Day 14/array.js
@@ -0,0 +1,59 @@
+// var jewelleryBox = ["Rings","Necklace","Earrings","Bracelet"]
+
+// console.log(jewelleryBox)
+// console.log(jewelleryBox[0])
+// console.log(jewelleryBox[3])
+
+// jewelleryBox.push("Watch","Chain")
+
+// jewelleryBox.pop()
+// jewelleryBox.pop()
+// jewelleryBox.pop()
+
+// jewelleryBox.shift()
+// jewelleryBox.shift()
+
+// jewelleryBox.unshift("Watch","Chain")
+// var jewelleryBox = ["Rings", "Necklace", "Earrings", "Bracelet"];
+
+// var newArray = jewelleryBox.slice(-3,3)
+// console.log(newArray)
+
+// jewelleryBox.splice(index, count, "","")
+// console.log(jewelleryBox.splice(3,1))
+// jewelleryBox.splice(2,1,"Watch", "Chain")
+// jewelleryBox.splice(5,2,"Watch", "Chain")
+// console.log(jewelleryBox)
+// var jewelleryBox = ["Rings", "Necklace", "Earrings", "Bracelet"];
+// console.log(jewelleryBox.indexOf("Earrings"))
+
+// console.log(jewelleryBox.join("-jwl "))
+// console.log(jewelleryBox.)
+
+// var string = "javaScript"; // tpircSavaj
+// console.log(string.split("").reverse().join(""))
+
+// var smallBox = ["pen","pencil"];
+
+// var bigBox = jewelleryBox.concat(smallBox)
+// console.log(bigBox)
+// var jewelleryBox = ["Rings", "Necklace", "Earrings", "Bracelet"];
+
+// // console.log(jewelleryBox.length)
+
+// for (var i = 0; i < jewelleryBox.length; i++) {
+//     console.log(jewelleryBox[i].length)
+//     console.log(jewelleryBox[i].length>5)
+
+//   if (jewelleryBox[i].length > 5) {
+//     console.log(jewelleryBox[i]);
+//   }
+// }
+
+// var num = [2,5,6,7,8,9];
+// for(var i=0; i<num.length;i++){
+//     // console.log(num[i]);
+//     if(num[i]%2==0){
+//         console.log(num[i])
+//     }
+// }
