fc0c0e1f0ae85d42b95c53722ea005f430dbc82e
Subbu1h2 <swamykola98765@gmail.com>
2024-11-27 12:05:22 +0530

    .

--- Node/Day 02/index.js
+++ Node/Day 02/index.js
@@ -6,5 +6,29 @@ let data = require('./app.js')
 
 // console.log(data)
 // console.log(data.age)
-console.log(data.studentData())
-console.log(data.student.name)
+// console.log(data.studentData())
+// console.log(data.student.name)
+
+let fs  = require('fs')
+// console.log (fs)
+
+fs.writeFile("index.txt","Hello from the node js", ()=>{
+    // console.log("file has created successfully");
+    
+} )
+fs.readFile("index.txt",'utf8',(err,data) =>{
+//    console.log(data);
+   
+})
+fs.unlink("index.txt",()=>{
+    // console.log("this file has deleted");
+    
+})
+
+let path = require('path')
+// console.log(path);
+
+let filePath = path.join('folder','subfolder','file.txt')
+// console.log(filePath);
+console.log(path.extname(filePath));
+
